e24e85546b95336d010d5827ff85d6c8b3a31f8b
RooHaaN7 <117039607+RooHaaN7@users.noreply.github.com>
2025-04-10 15:18:40 +0530

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,2 @@
-from transformers import DistilBertForSequenceClassification, DistilBertTokenizerFast
-
 model = DistilBertForSequenceClassification.from_pretrained("rohanN07/fake-news")
 tokenizer = DistilBertTokenizerFast.from_pretrained("rohanN07/fake-news")
