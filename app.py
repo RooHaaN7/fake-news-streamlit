88e58d4640c926a955ed3f58e1107a729fa460b6
RooHaaN7 <117039607+RooHaaN7@users.noreply.github.com>
2025-04-17 13:11:49 +0530

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from transformers import pipeline, DistilBertForSequenceClassification, DistilBertTokenizerFast
+
 # Page config
 st.set_page_config(page_title="Fake News Detector", page_icon="🧠", layout="centered")
 
